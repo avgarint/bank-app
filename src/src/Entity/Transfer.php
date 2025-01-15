15b3c7bb989254680a32ba321db7dc8d7e077c78
sami7453 <145992280+sami7453@users.noreply.github.com>
2025-01-15 10:04:12 +0100

    added transfer entity

---
+++ src/src/Entity/Transfer.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TransferRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TransferRepository::class)]
+class Transfer
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $no_account_emitter = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $no_account_receiver = null;
+
+    #[ORM\Column]
+    private ?int $amount_transfer = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNoAccountEmitter(): ?string
+    {
+        return $this->no_account_emitter;
+    }
+
+    public function setNoAccountEmitter(string $no_account_emitter): static
+    {
+        $this->no_account_emitter = $no_account_emitter;
+
+        return $this;
+    }
+
+    public function getNoAccountReceiver(): ?string
+    {
+        return $this->no_account_receiver;
+    }
+
+    public function setNoAccountReceiver(string $no_account_receiver): static
+    {
+        $this->no_account_receiver = $no_account_receiver;
+
+        return $this;
+    }
+
+    public function getAmountTransfer(): ?int
+    {
+        return $this->amount_transfer;
+    }
+
+    public function setAmountTransfer(int $amount_transfer): static
+    {
+        $this->amount_transfer = $amount_transfer;
+
+        return $this;
+    }
+}
