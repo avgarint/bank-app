a7911b27a2acd55e2cf38742ffadc0c6f69a08c1
sami7453 <145992280+sami7453@users.noreply.github.com>
2025-01-15 09:50:18 +0100

    added deposit entity

---
+++ src/src/Entity/Deposit.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DepositRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DepositRepository::class)]
+class Deposit
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $no_account_involve = null;
+
+    #[ORM\Column]
+    private ?int $amount_deposit = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNoAccountInvolve(): ?string
+    {
+        return $this->no_account_involve;
+    }
+
+    public function setNoAccountInvolve(string $no_account_involve): static
+    {
+        $this->no_account_involve = $no_account_involve;
+
+        return $this;
+    }
+
+    public function getAmountDeposit(): ?int
+    {
+        return $this->amount_deposit;
+    }
+
+    public function setAmountDeposit(int $amount_deposit): static
+    {
+        $this->amount_deposit = $amount_deposit;
+
+        return $this;
+    }
+}
