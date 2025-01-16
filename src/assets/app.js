06e6e9ead1139c4e920347990f7905a3b8697f46
Avgarint <66020831+avgarint@users.noreply.github.com>
2025-01-16 11:43:04 +0100

    Added graph to test

--- src/assets/app.js
+++ src/assets/app.js
@@ -8,3 +8,30 @@ import './bootstrap.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+//
+
+addEventListener('DOMContentLoaded', () => {
+  const canvases = document.querySelectorAll('canvas');
+  canvases.forEach(canvas => {
+      new Chart(canvas, {
+          type: 'line',
+          data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              borderWidth: 1,
+              tension: 0.3
+            }]
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true
+              }
+            }
+          }
+        });
+  });
+});
